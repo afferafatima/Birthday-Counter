e807bde8ebf4276ed9397bbbd3d23defe55c102f
Afeera Fatima <120155223+afferafatima@users.noreply.github.com>
2024-02-11 23:14:03 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 11, 2023',
+  birthdate: 'Feb 14, 2024',
   name: "Zaineb"
 };
 
